166498f1602711353e71effe67bab2bce1725ef6
sudomaaz <maazm0649@gmail.com>
2021-06-01 17:43:34 +0530

    updated stripe payment

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,42 +1,34 @@
-import React from 'react'
-import {useDispatch , useSelector} from 'react-redux'
-import StripeCheckout from 'react-stripe-checkout'
-import { placeOrder } from '../actions/orderActions'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import StripeCheckout from "react-stripe-checkout";
+import { placeOrder } from "../actions/orderActions";
 import Error from "../components/Error";
 import Loading from "../components/Loading";
-import Success from '../components/Success'
-export default function Checkout({subtotal}) {
+import Success from "../components/Success";
+export default function Checkout({ subtotal }) {
+  const orderstate = useSelector((state) => state.placeOrderReducer);
+  const { loading, error, success } = orderstate;
+  const dispatch = useDispatch();
+  function tokenHander(token) {
+    console.log(token);
+    dispatch(placeOrder(token, subtotal));
+  }
 
-    const orderstate = useSelector((state) => state.placeOrderReducer)
-    const {loading , error , success} = orderstate
-    const dispatch = useDispatch()
-    function tokenHander(token)
-    {
-        console.log(token);
-        dispatch(placeOrder(token , subtotal))
+  return (
+    <div>
+      {loading && <Loading />}
+      {error && <Error error="Something went wrong" />}
+      {success && <Success success="Your Order Placed Successfully" />}
 
-    }
-
-    return (
-        <div>
-            
-            {loading && (<Loading/>)}
-            {error && (<Error error='Something went wrong'/>)}
-            {success && (<Success success='Your Order Placed Successfully'/>)}
-
-            <StripeCheckout
-            amount={subtotal*100}
-            shippingAddress
-            token={tokenHander}
-            stripeKey='pk_test_51IYnC0SIR2AbPxU0TMStZwFUoaDZle9yXVygpVIzg36LdpO8aSG8B9j2C0AikiQw2YyCI8n4faFYQI5uG3Nk5EGQ00lCfjXYvZ'
-            currency='INR'
-            >
-
-                  
-                  <button className='btn'>Pay Now</button>
-
-            </StripeCheckout>
-            
-        </div>
-    )
+      <StripeCheckout
+        amount={subtotal * 100}
+        shippingAddress
+        token={tokenHander}
+        stripeKey="pk_test_51IxVZhSBzvuryKkMsc5QPEJOCk0ChVCfONzBYQjFJaetFILS8ZavFCaP37kVUBNHZdsOcRQ7RTgiVe7D2qeVZBOj00BZBsRzN9"
+        currency="INR"
+      >
+        <button className="btn">Pay Now</button>
+      </StripeCheckout>
+    </div>
+  );
 }
--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
 const stripe = require("stripe")(
-  "pk_test_51IxVZhSBzvuryKkMsc5QPEJOCk0ChVCfONzBYQjFJaetFILS8ZavFCaP37kVUBNHZdsOcRQ7RTgiVe7D2qeVZBOj00BZBsRzN9"
+  "sk_test_51IxVZhSBzvuryKkMyqEi872KCpnhbhTjpSkR7fk8ftDtAHNtYImqkseATY21jBovUNRgmodpj1uPWeDvxMHqpBxf00NxEVPIWE"
 );
 const Order = require("../models/orderModel");
 router.post("/placeorder", async (req, res) => {
